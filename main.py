27804cd77ce6d6daa655a45357a97d062c2ebcff
Atte Haataja <atte@kaswe.fi>
2023-12-07 18:11:04 +0900

    changed requirements and main

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+import av
 import streamlit as st
 import cv2
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
 
 class VideoTransformer(VideoTransformerBase):
-  def transform(self, frame):
+  def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
     img = frame.to_ndarray(format="bgr24")
 
     img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
     img = cv2.flip(img, 1)
 
-    return img
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 def main():
     st.title("WebRTC Example")
